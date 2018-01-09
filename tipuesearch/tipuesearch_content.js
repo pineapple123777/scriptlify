8dfc2a9f057c7426d51e84e6b60f0ffa54760b49
Tristian Kelly <tristian.kelly560@gmail.com>
2018-01-09 16:13:03 -0600

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -2,6 +2,6 @@
 ---
 var tipuesearch = {"pages": [
      {% for post in site.posts %}
-     {"title": "{{ post.title }}", "text": "{{ post.content | strip_html }}", "tags": "{{ post.categories }}", "url": "https://scriptlify.netlify.com{{ post.url }}"},
+     {"title": "{{ post.title }}", "text": {{ post.content | strip_html | strip_newlines | jsonify }}, "tags": "{{ post.categories }}", "url": "https://scriptlify.netlify.com{{ post.url }}"},
      {% endfor %}
 ]};
